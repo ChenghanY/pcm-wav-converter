c3c58b742b776f714e53faeff52a6a0549b8c06e
chenghanY <chenghan_yang@163.com>
2024-06-23 18:41:29 +0800

    Path.resolve() 简化为字符串传参

--- src/test/java/com/james/common/PathConstant.java
+++ src/test/java/com/james/common/PathConstant.java
@@ -13,21 +13,21 @@ public class PathConstant {
     /**
      * 从远端下载的 pcm 示例文件
      */
-    public static final Path SAMPLE_PCM_FILE = DIR_ROOT.resolve(Paths.get("sample.pcm"));
+    public static final Path SAMPLE_PCM_FILE = DIR_ROOT.resolve("sample.pcm");
 
     /**
      * pcm 输出文件
      */
-    public static final Path OUTPUT_PCM_FILE = DIR_ROOT.resolve(Paths.get("output.pcm"));
+    public static final Path OUTPUT_PCM_FILE = DIR_ROOT.resolve("output.pcm");
 
     /**
      * wav 输出文件
      */
-    public static final Path OUTPUT_WAV_FILE = DIR_ROOT.resolve(Paths.get("output.wav"));
+    public static final Path OUTPUT_WAV_FILE = DIR_ROOT.resolve("output.wav");
 
 
     /**
      * wav 输出文件 (经过pcm时长加倍处理)
      */
-    public static final Path OUTPUT_DOUBLE_LENGTH_WAV_FILE = DIR_ROOT.resolve(Paths.get("output-double-length.wav"));
+    public static final Path OUTPUT_DOUBLE_LENGTH_WAV_FILE = DIR_ROOT.resolve("output-double-length.wav");
 }
